46d04b85ab72763895060695f79a5d988258d351
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-16 14:47:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 http = require('node:http')
 // Creating a server
 server = http.createServer((req, res) =>{
-    res.writeHead(404)
-    res.end("hey there")
+    res.writeHead(200, {"Content-Type":"text/plain"})
+    res.end("<h1>hey there</h1>")
 })
 
 // server response

36a56d9a123aa816957d1b14d35eed8ab16d78a6
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-16 12:11:20 +0530

    Update fs(write).js

--- fs(write).js
+++ fs(write).js
@@ -2,7 +2,7 @@ fs = require('node:fs')
 // writing file system syncronously: code below
 // fs.writeFileSync('./b.txt', 'hasmukhwa')
 
-// writing file asyncrhonously and updating(append): code below
+// writing file asyncrhonously and updating(append-): code below
 fs.writeFile('./c.txt', 'pyara hasmukh damodar',{flag: "a"},(error)=>{
     if(error){console.log(error)}
     else{console.log('written')}

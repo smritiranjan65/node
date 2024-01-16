147a1d63fa101f0cc114fefefe9f6344be24eef8
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-16 14:34:51 +0530

    server

--- ser.js
+++
@@ -1,8 +0,0 @@
-a = require('node:http')
-
-server = a.createServer((req,res) => {
-    res.writeHead(404)
-    res.end("<h1 style='color:red; text-align:center' >this is my first node server</h1>")
-})
-
-server.listen(3000, ()=>{console.log('server running')})
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 http = require('node:http')
+// Creating a server
+server = http.createServer((req, res) =>{
+    res.end("hey there")
+})
+
+// server response
+
+server.listen(3000, ()=>{console.log('server up')})

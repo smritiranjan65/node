ee752b1b1dd8236524c7a29d704c5f06fcc0bcfe
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-23 10:53:02 +0530

    Update stream(buffer).js

--- stream(buffer).js
+++ stream(buffer).js
@@ -1,6 +1,6 @@
 buffer = new Buffer.from("nashit")
 
 buffer.write("abcdefghijklmnopqrstuvwxyz")
-console.log(buffer.toJSON())
+//console.log(buffer.toJSON())
 console.log(buffer.toString())
-console.log(buffer)
+//console.log(buffer)

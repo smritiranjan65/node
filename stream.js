f31180ac7a42b426bbf66dd82303ca152d59e2f2
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-23 11:24:16 +0530

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,6 +1,12 @@
 fs = require('node:fs')
+zlib = require('node:gzip')
+
+gzip=zlib.createGzip()
+
 // Readable stream
 readableStream = fs.createReadStream('./new.txt', {encoding:'utf-8', highWaterMark: 6})
+// zipping files
+readableStream.pipe(gzip).pipe(fs.WriteStream('./knp.txt.gz'))
 
 // Writable stream
 writeableStream = fs.createWriteStream('./patna.txt')

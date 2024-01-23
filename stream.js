04dd315a4018601178f36be0bee8ca1a2d11f5aa
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-23 11:04:22 +0530

    added

--- stream.js
+++ stream.js
@@ -2,4 +2,12 @@ fs = require('node:fs')
 
 readableStream = fs.createReadStream('./new.txt', {encoding:'utf-8'})
 
+writeableStream = fs.createWriteStream('./patna.txt')
+
+readableStream.on("data", (chunk )=>{
+
+    console.log(chunk)
+    writeableStream.write(chunk)
+})
+
 console.log(readableStream)

5975585ca63a664e25183862cad72657c524cd50
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-01-24 10:07:52 +0530

    mongo connection

--- stream/stream.js
+++ stream/stream.js
@@ -1,17 +1,23 @@
 fs = require('node:fs')
-zlib = require('node:gzip')
+zlib = require('node:zlib')
 
 gzip=zlib.createGzip()
 
 // Readable stream
-readableStream = fs.createReadStream('./new.txt', {encoding:'utf-8', highWaterMark: 6})
+
+readableStream = fs.createReadStream('./new.txt', {encoding:'utf-8'})
+
 // zipping files
 readableStream.pipe(gzip).pipe(fs.WriteStream('./knp.txt.gz'))
 
 // Writable stream
-writeableStream = fs.createWriteStream('./patna.txt')
+
+//writeableStream = fs.createWriteStream('./patna.txt')
+
 // piping
-readableStream.pipe(writeableStream)
+
+//readableStream.pipe(writeableStream)
+
 // creating chunks
 //readableStream.on("data", (chunk )=>{
 
